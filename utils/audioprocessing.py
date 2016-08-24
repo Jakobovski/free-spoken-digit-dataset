b52b2621d835ef1c921e0e55de2a8389cd42472e
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2016-08-24 15:14:00 +0200

    adapted pip requirements to the current state of the code, more coding.

--- utils/audioprocessing.py
+++ utils/audioprocessing.py
@@ -8,11 +8,9 @@
 @license : BSD-2-Clause
 '''
 
-
-import scipy.io.wavfile as wavfile
 import numpy as np
 
-def stft(samples, framesize = 512,step = 64, windowfxn = np.hanning):
+def stft(samples, framesize = 512,step = 16, windowfxn = np.hanning):
     '''
     Computes the short time fourier transform of a given single channel wav signal.
 
@@ -26,7 +24,7 @@ def stft(samples, framesize = 512,step = 64, windowfxn = np.hanning):
             the window size of a time frame to process via fft
 
         step : int
-            default = 64
+            default = 16
             the offset between two consecutive time windows
 
         windowfxn : function
@@ -45,7 +43,7 @@ def stft(samples, framesize = 512,step = 64, windowfxn = np.hanning):
     Ww = 1 #don't fuck around with this for now.
     return [np.fft.fft(Ww * samples[offset:offset+framesize]) for offset in xrange(0,len(samples)-framesize,step)]
 
-def istft(frequencies, step=64, windowfxn = np.hanning, dtype = None):
+def istft(frequencies, step=16, windowfxn = np.hanning, dtype = None):
     '''
     Computes the inverse short time fourier transform from a given sequence of frequencies and restores the described wav signal
 
@@ -55,7 +53,7 @@ def istft(frequencies, step=64, windowfxn = np.hanning, dtype = None):
         a list of N complex valued numpy arrays containing the frequency spectrums of consecutive time windows.
 
     step : int
-        default = 64
+        default = 16
         the offset in amount of samples between the fourier-transformed input frequency spectra
 
     windowfxn : function
@@ -102,59 +100,145 @@ def istft(frequencies, step=64, windowfxn = np.hanning, dtype = None):
 
     return samples
 
-
-
-
-def wav2png(wavpath,pngpath=None, framesize = 512, step = 64, windowfxn = np.hanning, layer3constant = 0):
+def samples2freqmat(samples, framesize = 512, step = 16, windowfxn = np.hanning ):
     '''
-    Computes a complex-valued spectrogram from the wav file specified by wavpath,
-    where
-        the R-layer of the output png holds the real valued spectrum,
-        the G-layer of the output png holds the imaginarily valued spectrum,
-        and the B-layer is constantly 0
+    Computes a complex-valued spectrogram using short time fourier transform (stft) based on a
+    wav file and produces a png file and stores the real and imaginary parts of the data as
+    separate layers in matrix form.
 
-        framesize is the time frame to consider
-        spectrogram_dim is the number of time and frequency steps to output.
-        step controls the overlap of time frames to spectrograph.
-        windowfxn defines a window of weights applied to the raw singal for a smoother transition and later easier reconstruction
+    Parameters:
+    -----------
+        samples: numpy.ndarray
+            the input sequence wav samples
 
-        returns the image and a parameter dictionary
+        framesize: int
+            default = 512
+            the size of the sequency time frame to consider for stft.
+
+        step: int
+            default = 16
+            the offset in number of samples between two consecutive frame windows used in stft
+
+        windowfxn: function
+            default = numpy.hanning
+            CAUTION! NOT USED BY NEITHER STFT AND ISTFT CURRENTLY
+            function for computing weights for the samples of each time window used in stft
+
+    Returns:
+    --------
+        freqmat : numpy.ndarry
+            Numpy array of shape (framesize,T,2)
+            where framesize is the number of extracted frequencies, T is the number of transformed
+            time steps and the third dimension indexing the real (index 0) and imaginary (index 1)
+            components of the data.
+            The order of the frequency components conforms to the implementation of numpy.fft.fft,
+            e.g. for each t:
+            freqmat[t,0,:] contains the signal mean (zero frequency term)
+            freqmat[t,1:n/2,:] contains the positive fequency terms
+            freqmat[t,n/2+1:,:] contains the negative frequency terms
+            freqmat[t,n,:] contains the Nyquist frequency for n%2 == 1
+            freqmat[t,(n-1)/2,:] contains the largest positive frequency for n%2 == 0
+            freqmat[t,(n+1)/2,:] contains the largest negative frequency for n%2 == 0
+
     '''
 
-    samplerate,samples = wavfile.read(wavpath) #dtype of samples is int16
-    T = len(samples)
-    frequency_data = np.zeros((framesize,framesize,3));
     # some debugging values for myself.
     #overlap = (framesize - step + 0.0) / framesize
     #framesize = double the extractable frequency spectrum?
     #how does that relate to - with the signal - the output dims (how much signal can we get, what needs to be padded?)
-
-    raw_frequency_data = stft(samples, framesize,step,windowfxn)
-    resample = istft(raw_frequency_data,step)
-    resample_tenth = istft([raw_frequency_data[i]/10. for i in xrange(len(raw_frequency_data))],step)
-
-    #wavfile.write('original.wav', samplerate, samples)
-    #wavfile.write('restored.wav', samplerate, resample.astype(np.int16)) # ROUNDING TO INT IS FUCKING IMPORTANT: DENOISING DELUXE!!!
-    #wavfile.write('restored_tenth.wav', samplerate, resample_tenth.astype(np.int16)) # FREQUENCY MUTING ON LOG SCALE WITH HEATMAPS?
+    T = len(samples)
+    raw_frequency_data = np.array(stft(samples, framesize,step,windowfxn)).T # framesize x N numpy.ndarray of complex128
+    freqmat = np.empty((framesize, raw_frequency_data.shape[1],2)) # float64
+    freqmat[...,0] = np.real(raw_frequency_data)
+    freqmat[...,1] = np.imag(raw_frequency_data)
+    return freqmat
 
 
+def freqmat2samples(freqmat, step=16, windowfxn= np.hanning, dtype = None):
+    '''
+    Inverse function to samples2freqmat. See function description for details
 
-    #AS IMAGES BELOW HERE
-    #print 'debug'
-    #print '...'
+    Parameters:
+    -----------
+    freqmat : numpy.ndarray
+        A shaped numpy matrix describing the frequency spectrum shaped (F,T,2+)
+        F is the frequency frame size (or 2*frequencies or the length of the time frame of the signal sequences described)
+        T is the number of consecutive time windows encoded in the matrix
+        the last dimension encodes the real (index 0) and imaginary (index 1) parts of the spectra.
+        additional layers of this index are ignored
 
-    #for i in xrange(len(raw_frequency_data)):
-    #    tspectrum = raw_frequency_data[i]
+    step : int
+        default = 16
+        the offset in amount of samples between the fourier-transformed input frequency spectra
 
-    #    frequency_data[:,i,0] = np.real(tspectrum)
-    #    frequency_data[:,i,1] = np.imag(tspectrum)
-    #    frequency_data[:,i,2] = layer3constant
+    windowfxn : function
+        default = numpy.hanning. CURRENTLY INACTIVE. IDENTITY FUNCTION IS USED!!!
+        A wave function for weighting the samples of a time window for a smoother recovery frequency spectrum
+        This should be the same wave function as used during the stft
 
-        #NOTE HERE:
-        # real(tspectrum[x]) == real(tspectrum[-x])
-        # BUT
-        # imag(tspectrum[x]) == - imag(tspectrum[-x])
-        # use this knowledge somehow? compress the data with that, later reconstruct? gotta pee.
+    dtype : a numpy data type constant
+        default = None
+        defines the target data type of the output wave signal. The default value None returns the wav samples
+        as directly restored by the ISTFT algorithm, i.e samples.dtype = numpy.float64.
+        However, due to residual reconstruction errors very small deviations from the original signal might be
+        the result, resulting in strong auditory noise due to noisy high frequency content being embedded.
+        A Recommended value for dtype would be numpy.int16
+
+    Returns:
+    --------
+        samples : np.ndarray
+            the restored wav signal as a numpy array of shape (N,)
+    '''
+
+    REAL = freqmat[...,0].T # T x F shaped real frequency components
+    IMAG = freqmat[...,0].T # T x F shaped imaginary frequency components
+
+    frequencies = [REAL[t,:] + 1j*IMAG[t,:] for t in xrange(REAL.shape[0])]
+    samples = istft(frequencies, step, windowfxn, dtype)
+    return samples
 
 
 
+
+
+
+'''
+RECORDING FUNCTIONS BELOW
+'''
+
+
+
+def trim_silence(samples, noise_threshold=150):
+    '''
+    Removes leading and trailing periods of silence from a given audio signal.
+    This impmentation has been adapted from: https://github.com/Jakobovski/free-spoken-digit-dataset/blob/master/utils/trimmer.py
+
+    Parameters:
+    -----------
+        samples : numpy.ndarray
+            a one-dimensional numpy array
+
+        noise_threshold : int
+            thresholding value. every sample at the beginning and end of samples smaller
+            than this value will be considered as noise/silence.
+
+    Returns:
+    --------
+        trimmed_samples: numpy.ndarray
+            the trimmed audio samples without leading and trailing noise segments.
+    '''
+
+    start,end = None,None
+    for t,value in enumerate(samples):
+        if abs(value) > noise_threshold:
+            start = t;
+            break
+    for t,value in enumerate(samples[::-1]):
+        if abs(value) > noise_threshold:
+            end = t;
+            break
+
+    return samples[start:end+1]
+
+
+def record_audio(TODO = 'TODO'): pass
--- utils/testing.py
+++ utils/testing.py
@@ -8,7 +8,39 @@
 @license : BSD-2-Clause
 '''
 
+import scipy.io.wavfile as wavfile
+import numpy as np
 import audioprocessing
 
+framesize = 512
+step = 16
+
 wavpath = '/media/lapuschkin/Data/AUDIO-LRP/free-spoken-digit-dataset/recordings_8000/0_jackson_10.wav'
-audioprocessing.wav2png(wavpath)
+samplerate,samples = wavfile.read(wavpath) #dtype of samples is int16
+T = len(samples)
+raw_frequency_data =    audioprocessing.stft(samples,framesize,step)
+resample =              audioprocessing.istft(raw_frequency_data,step,dtype=np.int16)
+resample_tenth =        audioprocessing.istft([raw_frequency_data[i]/10. for i in xrange(len(raw_frequency_data))],step,dtype=np.int16)
+resample_three =        audioprocessing.istft([raw_frequency_data[i]*3 for i in xrange(len(raw_frequency_data))],step,dtype=np.int16)
+wavfile.write('original.wav', samplerate, samples)
+wavfile.write('restored.wav', samplerate, resample)
+wavfile.write('restored_tenth.wav', samplerate, resample_tenth)
+wavfile.write('restored_three.wav', samplerate, resample_three)
+
+
+freqmat = audioprocessing.samples2freqmat(samples, framesize, step )
+restoration =  audioprocessing.freqmat2samples(freqmat,step,dtype=np.int16)
+wavfile.write('matrix_restored.wav',samplerate,restoration)
+print 'done' # RESTORATION DIFFERS FROM RESAMPLE: FIND OUT WHY . HAS TO DO WITH FREQ -> WAV
+
+
+#NOTE HERE:
+        # real(tspectrum[x]) == real(tspectrum[-x])
+        # BUT
+        # imag(tspectrum[x]) == - imag(tspectrum[-x])
+        # use this knowledge somehow? compress the data with that, later reconstruct? gotta pee.
+print 'DURING POST PROCESSING FEATURE EXTRACTION:'
+print '- crop negative frequencies?, restore later? what do the heatmaps say?' #maybe
+print '- pad signal before spectrum analysis?' #no
+print '- only compute spectrogram, save phase information for later recovery?' #maybe, kinda not
+print '- linearly scaling the frequency == linearly scaling the samples by the same factor'
